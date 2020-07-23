5b5c1693ad5015b489a50fa285fc1248a1ffc4fd
SafdarDabeerKhan <safdar.khan.ext@bayer.com>
2020-07-23 12:01:47 +0200

    hateos stuff added

--- restful-web-services/src/main/java/com/test/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/test/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,13 +1,18 @@
 package com.test.rest.webservices.restfulwebservices.user;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import java.net.URI;
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,14 +32,11 @@ public class UserResource {
 		return userDaoService.findAll();
 	}
 
-	@GetMapping("/users/{id}")
-	public User retrieveUser(@PathVariable int id) {
-		User user = userDaoService.findOne(id);
-		if(user == null)
-			throw new UserNotFoundException("id-" + id);
-		return user;
-	}
-
+	/*
+	 * @GetMapping("/users/{id}") public User retrieveUser(@PathVariable int id) {
+	 * User user = userDaoService.findOne(id); if (user == null) throw new
+	 * UserNotFoundException("id-" + id); return user; }
+	 */
 	@PostMapping(path = "/users")
 	public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
 		User savedUser = userDaoService.save(user);
@@ -42,13 +44,30 @@ public class UserResource {
 				.toUri();
 		return ResponseEntity.created(location).build();
 	}
-	
+
 	@DeleteMapping("/users/{id}")
 	public void deleteUser(@PathVariable int id) {
 		User user = userDaoService.deleteById(id);
-		if(user == null)
+		if (user == null)
 			throw new UserNotFoundException("id-" + id);
 	}
 
+	@GetMapping("/users/{id}")
+	public EntityModel<User> retrieveUserUsingHATEOAS(@PathVariable int id) {
+		User user = userDaoService.findOne(id);
+		if (user == null)
+			throw new UserNotFoundException("id-" + id);
+
+		// "all-users", SERVER_PATH + "/users"
+		// retrieveAllUsers
+
+		EntityModel<User> resource = EntityModel.of(user);
+
+		WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		resource.add(linkTo.withRel("all-users"));
+		// HATEOS
+
+		return resource;
+	}
 
 }
